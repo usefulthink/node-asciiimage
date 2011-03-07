472d027ffe4b5aab97d0ed72cee50547d0f48316
Martin Schuhfuss <m.schuhfuss@googlemail.com>
2011-03-07 23:35:08 +0100

    - more-refactoring, removed color-support and all html-stuff

--- ascii.js
+++ ascii.js
@@ -1,122 +1,77 @@
+var sys = require('sys'),
+	Canvas = require('canvas'),
+	Image = Canvas.Image;
 
-var scales = {
-	defaultScale : ' .,:;i1tfLCG08@'.split(''),
-	variant2 : '@%#*+=-:. '.reverse().split(''),
-	variant3 : '#¥¥®®ØØ$$ø0oo°++=-,.    '.reverse().split(''),
-	variant4 : '#WMBRXVYIti+=;:,. '.reverse().split(''),
-	'ultra-wide' : ('MMMMMMM@@@@@@@WWWWWWWWWBBBBBBBB000000008888888ZZZZZZZZZaZaaaaaa2222222SSS' +
-			'SSSSXXXXXXXXXXX7777777rrrrrrr;;;;;;;;iiiiiiiii:::::::,:,,,,,,.........       ').reverse().split(''),
-	wide : '@@@@@@@######MMMBBHHHAAAA&&GGhh9933XXX222255SSSiiiissssrrrrrrr;;;;;;;;:::::::,,,,,,,........        '.reverse().split(''),
-	hatching : '##XXxxx+++===---;;,,...    '.reverse().split(''),
-	bits : '# '.reverse().split(''),
-	binary : '01 '.reverse().split(''),
-	greyscale : '"█▓▒░ '.reverse().split(''),
+var img = new Image();
+img.onload = function() {
+	renderImage(img);
+}
+img.src = __dirname + '/arnd.jpg';
 
-    color : ' CGO08@'.split('')
-};
 
-function renderImage(options) {
+
+function renderImage(image, options) {
+	var brightnessMappings = {
+		defaultScale : ' .,:;i1tfLCG08@'.split(''),
+	//	variant2 : '@%#*+=-:. '.reverse().split(''),
+	//	variant3 : '#¥¥®®ØØ$$ø0oo°++=-,.    '.reverse().split(''),
+	//	variant4 : '#WMBRXVYIti+=;:,. '.reverse().split(''),
+	//	wide : '@@@@@@@######MMMBBHHHAAAA&&GGhh9933XXX222255SSSiiiissssrrrrrrr;;;;;;;;:::::::,,,,,,,........        '.reverse().split(''),
+		x:         '    ...,,:;---===+++xxxXX##@@'.split('')
+	//	bits : ' #'.split(''),
+	//	binary : ' 10'.split('')
+	};
+	
 	options = options || {};
 	
-	// settings : prefer options over attributes over defualts
-	var scale = options.scale || 1; 
-	var isColor = options.color || false;
-
-	// FIXME: we should be able to retrieve this from the image-file
-	var isAlpha = options.alpha || false;
-	var isBlock = options.block || false;
-	var isInverted = options.invert || false;
 	// factor by which pixels are mapped to a number of characters
-	var resolution = options.resolution || 0.5;
-	var characters = options.characters || (isColor ? scales.color : scales.defaultScale);
-	
-	// FIXME: rename
-	switch (resolution) {
-		case "low": resolution = 0.25; break;
-		case "medium": resolution = 0.5; break;
-		case "high": resolution = 1; break;
-	}
-	
+	var resolution = options.resolution || 0.1;
+	var characters = brightnessMappings.x;
+	var imageWidth = image.width, imageHeight = image.height,
+			width = Math.round(imageWidth * resolution),
+			height = Math.round(imageHeight * resolution);
 
-	// FIXME
-	//  - retrieve those values from somwhere (jhead or something)!
-	//  - find out how node-canvas needs those values
-	var imageFile, imageWidth, imageHeight;
-
-	//setup our resources
-	var width = Math.round(imageWidth * resolution);
-	var height = Math.round(imageHeight * resolution);
-
-	// FIXME: implement gimmeTheCanvas using @tjholowaychucks node-canvas-foo
-	var canvas = gimmeTheCanvas(width, height);
+	var canvas = new Canvas(width, height);
 	var context = canvas.getContext('2d');
 
-	// FIXME: since characters don't have an aspect-ratio of 1, the image should be stretched
-	//   accordingly when drawing to the canvas (this should produce much better results...)
-	context.drawImage(imageFile, 0, 0, width, height);
+	context.drawImage(image, 0, 0, width, height);
 
 	var data = context.getImageData(0, 0, width, height).data;
 	
-	// yeah, we'll get rid of you later...
-	var strChars = "<span>";
+	var chars = "";
 	var offset, // offset in imageData array
 		red, green, blue, alpha; // rgba
 
 	function getCharacterReplacement(red, green, blue, alpha) {
-		var strChars='', charScale=characters, thisChar=' ', characterPosition=0; brightness=0;
+		var chars='', charScale=characters, thisChar=' ', characterPosition=0; brightness=0;
 		
-		if(alpha !== 0) {
-			if(isAlpha) {
-				brightness = (0.3*red + 0.59*green + 0.11*blue) * (alpha/255) / 255;
-			} else {
-				brightness = (0.3*red + 0.59*green + 0.11*blue) / 255;
-			}
-			characterPosition = (charScale.length-1) - Math.round(brightness * (charScale.length-1));
+		if(alpha/255 < 0.3) {
+			return ' ';
 		}
+		
+		brightness = (0.3*red + 0.59*green + 0.11*blue) * (alpha/255) / 255;
+		characterPosition = (charScale.length-1) - Math.round(brightness * (charScale.length-1));
 
-		if(isInverted) {
-			characterPosition = (characters.length-1) - characterPosition;
-		}
-		
-		thisChar = characters[characterPosition];
-		
-		if (thisChar === ' ') {
-			thisChar = '&nbsp;';
-		}
-		
-		if (isColor) {
-			strChars += '<span style="'
-				+ 'color:rgb('+red+','+green+','+blue+');'
-				+ (isBlock ? 'background-color:rgb('+red+','+green+','+iBlue+');' : '')
-				+ (isAlpha ? 'opacity:' + (alpha/255) + ';' : '')
-				+ '">' + thisChar + '</span>';
-		} else {
-			if(isAlpha) {
-				if(alpha/255 < 0.3) {
-					strChars += ' ';
-				} else {
-					strChars += thisChar;
-				}
-			} else {
-				strChars += thisChar;
-			}
-		}
-		
-		return strChars;
+		return charScale[characterPosition];
 	}
 
-	for (var y=0; y<height; y+=2) {
+	for (var y=0; y<height; y+=2) { // process every 2nd row
 		for (var x=0; x<width; x++) {
 			if(x%3 === 0) { continue; } // account for the mismatch in scale of a char, here we assume 3:2 ratio and correct to 2:2
 			offset = (y*width + x) * 4;
 			red = data[offset];
 			green = data[offset + 1];
 			blue = data[offset + 2];
+
+			// FIXME: if we load an opaque image, do we need to map the alpha-channel? or is it always 255?
+			// TODO: make sure this is 255 for opaque pixels
 			alpha = data[offset + 3];
 			
-			strChars += getCharacterReplacement(red, green, blue, alpha);
+			chars += getCharacterReplacement(red, green, blue, alpha);
 		}
-		strChars += '</span><br/><span>';
+		chars += '\n';
 	}
+
+	sys.puts(chars);
 }
 
